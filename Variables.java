730a95c62557f40257b10a0aad5eab043db62ef3
Jerald-CS <jeraldhuang26@crosspointacademy.org>
2025-08-20 19:39:12 -0700

    It works

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+    public static void main (String[] args){
+        int x;
+        x = 1;
+        x++;
+        System.out.println(x);
+        double b = 3.3339;
+        System.out.println(b);
+        String greeting = "Hello, world!";
+        System.out.println(greeting);           // prints Hello, world!
+        System.out.println(greeting.length());  // prints 13
+        System.out.println(greeting.toUpperCase()); // prints HELLO, WORLD!
+        System.out.println(greeting.charAt(0)); // prints H
+    }
+}
